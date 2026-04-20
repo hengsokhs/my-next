d14df9477ab09aa8585e32296bcecf43c04f32b7
WINGMONEY\sok.heng <sok.heng@wingbank.com.kh>
2026-04-20 17:04:17 +0700

    add db and prisma

--- app/user/loading.tsx
+++ app/user/loading.tsx
@@ -28,9 +28,9 @@ export default function Loading() {
             <h1 className="text-4xl font-bold mb-8 text-green-600 flex justify-center">
                 <span>Loading</span>
                 <span className="flex gap-px mx-3">
-                  <span className="animate-bounce" style={{animationDelay: "0ms"}}>.</span>
-                  <span className="animate-bounce" style={{animationDelay: "150ms"}}>.</span>
-                  <span className="animate-bounce" style={{animationDelay: "300ms"}}>.</span>
+                    <span className="animate-bounce" style={{ animationDelay: "0ms" }}>.</span>
+                    <span className="animate-bounce" style={{ animationDelay: "150ms" }}>.</span>
+                    <span className="animate-bounce" style={{ animationDelay: "300ms" }}>.</span>
                 </span>
             </h1>
         </main>
